2b6ab6016ac345ff463d58647230037978dbb7f9
Projectyuuri07 <projectyuuri@gmail.com>
2024-02-26 18:35:05 -0300

    Finalização da screen Load

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,10 +1,11 @@
 import Cadastro from './Screens/Cadastro';
+import Load from './Screens/Load';
 import Login from './Screens/Login';
 import RecupSenha from './Screens/RecupSenha';
 
 const App = () => {
   return (
-    <Login />
+    <Load />
   )
 }
 
---
+++ my-app/Screens/Load.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Status from '../components/StatusBar';
+import { FontAwesome } from '@expo/vector-icons';
+
+const Load = () => {
+  return (
+    <View style={styles.container}>
+      <Status title={''} />
+      <FontAwesome name="circle-o-notch" size={70} color="white" style={styles.circle} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  circle: {
+    marginTop: -40,  
+    transform: [{ rotate: '60deg' }],  
+  },
+});
+
+export default Load;
